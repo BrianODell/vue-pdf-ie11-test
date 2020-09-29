f2af02a0bd5798d4ac282b03872d86a36fd29357
Brian O'Dell <briantodell@gmail.com>
2020-09-29 16:55:14 -0400

    pdf render working in chrome

---
+++ pages/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <client-only>
+      <vue-pdf class="your-pdf-class" :src="pdfSrc" :page="1" @num-pages="pdfPageCount = $event"></vue-pdf>
+      <div v-if="pdfPageCount > 1">
+          <div v-for="(pageNum, index) in pdfPageCount " :key="index">
+              <vue-pdf class="your-pdf-class" :src="pdfSrc" :page="pageNum" v-if="pageNum > 1"></vue-pdf>
+          </div>
+      </div>
+    </client-only>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        pdfSrc: '/compressed.tracemonkey-pldi-09.pdf',
+        pdfPageCount : 0,
+    }
+  }
+}
+</script>
