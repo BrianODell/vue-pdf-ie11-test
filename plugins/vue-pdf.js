866d41fba58e0badd01fa838e0bb5f4508f75213
Brian O'Dell <briantodell@gmail.com>
2020-10-01 09:18:18 -0400

    added my node_modules I've been hacking on

--- plugins/vue-pdf.js
+++ plugins/vue-pdf.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
 import pdf from "vue-pdf/src/vuePdfNoSssNoWorker.vue";
 
+import(
+  /* webpackChunkName: "my-chunk-name" */
+  '~/node_modules/pdfjs-dist/build/pdf.worker.entry.js'
+);
+
 Vue.component('vue-pdf', pdf)
